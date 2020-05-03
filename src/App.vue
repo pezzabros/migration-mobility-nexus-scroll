571c8a0621648fb5c747701f7e2dffd582351015
Daniele Pezzatini <daniele@bestiario.org>
2020-05-03 17:19:04 +0200

    Update text and animations

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     <div class="intro">
       <h1>The Migration-Mobility Nexus</h1>
       <div class="authors">Lorenzo Piccoli and Matteo Gianni - <i>University of Neuchâtel and University of Geneva</i></div>
-      <p>
+      <!--<p-
         The Migration-MobiliObserve the interaction between different ways to describe human movementty Nexus is an analytical tool to:
         <ul>
           <li>Explain how this interaction provides meaning to social and political phenomena</li>
         </ul>
-      </p>
+      </p-->
     </div>
 
     <Scrollama
@@ -17,48 +17,58 @@
       @step-enter="({ element }) => (currStep = element.dataset.stepNo)"
     >
       <div class="step" data-step-no="1">
-        <h3>Continuum</h3>
-        <div class="subtitle">Fluid mobilities might gradually change into 
-          more stable forms of migration and the other way around.
-        </div>
-        <div class="content">
-          Commercial commitments in trade agreements foster longer term migration, rather than temporary forms of mobility [IP24] 
-The length of residency and the contributions that are required to benefit from the welfare state influence natives’ propensity to discriminate against migrants and movers [IP26–IP27]
-Recent changes in welfare policies have allowed individuals to move freely across countries of the Schengen area [IP22]
-        </div>
+        We live in a world on the move. Human movement, both within and across countries lies at the core of today’s social, economic and political life.  
+        <br/>Our work at the Nccr – on the move is borne out of the conviction that we should understand how these different forms of movement coexist, meet, overlap, and collide, through the Migration-Mobility Nexus.
       </div>
       <div class="step" data-step-no="2">
-        <h3>Enablement</h3>
-        <div class="subtitle">Migration requires mobility,
-while mobility can lead to migration.
-        </div>
-        <div class="content">
-          Inclusive policies and voting rights reduce the propensity of non-citizens to change their residency [IP30–IP31]
-Migration experiences can foster both internal mobility in the country and transnational business activities based on cross-border mobilities [IP23– IP32]
-        </div>
+        The Migration-Mobility Nexus works as a pair of lenses that one wears to make sense of a world in motion. It postulates a connection between different ways of framing human movement.
+      <br/><br/>
+      Of course, one can say that to speak about a Migration-Mobility Nexus we should first provide definitions of what ‘migration’ and ‘mobility’ are. 
+      <br/><br/>
+      But there is not one single definition of migration and mobility that all our researchers use or should use. In fact, the very definition depends upon the epistemological approach our researchers have chosen to follow to address their own particular question. 
+      <br/><br/>
+      Instead, it is precisely in avoiding postulating definitions from the outset that the heuristic power of the Nexus becomes relevant. It is through the analysis of different interplays between migration and mobility that specific definitions matter and fosters internally consistent researches.
       </div>
       <div class="step" data-step-no="3">
+        <h3>Continuum</h3>
+        <div class="subtitle">Fluid mobilities might gradually change into more stable forms of migration; and the other way around.
+        </div>
+        <div class="content">
+          Commercial commitments in trade agreements foster longer term migration, rather than temporary forms of mobility [IP24].
+          <br/><br/>The length of residency and the contributions that are required to benefit from the welfare state influence natives’ propensity to discriminate against migrants and movers [IP26–IP27].
+          <br/><br/>Recent changes in welfare policies have allowed individuals to move freely across countries of the Schengen area [IP22].
+        </div>
+      </div>
+      <div class="step" data-step-no="4">
+        <h3>Enablement</h3>
+        <div class="subtitle">Migration requires mobility, while mobility can lead to migration.
+        </div>
+        <div class="content">
+          Inclusive policies and voting rights reduce the propensity of non-citizens to change their residency [IP30–IP31–IP35] 
+          <br/><br/>Migration experiences can foster both internal mobility in the country and transnational business activities based on cross-border mobilities [IP23– IP32]
+        </div>
+      </div>
+      <div class="step" data-step-no="5">
         <h3>Opposition </h3>
-        <div class="subtitle">Migration and mobility 
-are two mutually exclusive conceptions of movement.
+        <div class="subtitle">Migration and mobility are two mutually exclusive conceptions of movement.
         </div>
         <div class="content">
           The mobility of undocumented migrants and refugees challenges and undermines migration regulations [IP26–IP28]
         </div>
       </div>
-      <div class="step" data-step-no="4">
+      <div class="step" data-step-no="6">
         <h3>Hierarchy</h3>
-        <div class="subtitle">Migration and mobility are political categoriesthat legitimize hierarchies of movement
+        <div class="subtitle">Migration and mobility are political categories that legitimize hierarchies of movement
         </div>
         <div class="content">
-          Temporary commercial mobility commitments in trade agreements may create privileges and disadvantages compared to traditional migration [IP24]
-In the labour market and in access to welfare, one’s migration status is negatively associated with the probability of being employed and of receiving benefits [IP25–IP29]
-Transnational families can be formed voluntarily, for lifestyle reasons, and involuntarily because of legal constraints, wars, health or financial issues [IP23–IP33]
-Migration is only one among many mobilities that shape localities in terms of inclusion and exclusion, transnationalisation, boundaries and people’s life trajectories [IP34]
+          Temporary commercial mobility commitments in trade agreements may create privileges and disadvantages compared to traditional migration [IP24] 
+          <br/><br/>In the labour market and in access to welfare, one’s migration status is negatively associated with the probability of being employed and of receiving benefits [IP25–IP29] 
+          <br/><br/>Transnational families can be formed voluntarily, for lifestyle reasons, and involuntarily because of legal constraints, wars, health or financial issues [IP23–IP33] 
+          <br/><br/>Migration is only one among many mobilities that shape localities in terms of inclusion and exclusion, transnationalisation, boundaries and people’s life trajectories [IP34]
         </div>
       </div>
       <div class="graphic" slot="graphic">
-        <chart :currentStep="currStep" :totalSteps="4"></chart>
+        <chart :currentStep="currStep" :totalSteps="6"></chart>
       </div>
     </Scrollama>
 
@@ -117,7 +127,7 @@ export default {
   margin: 0 3rem;
 .intro
   padding-top: 10px
-  padding-bottom 20vh
+  padding-bottom 10vh
   width 75%
 h1
   padding 0px
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,24 +1,92 @@
 <template>
   <div class="chart-steps">
-    <img src="~@/assets/images/main-chart.svg">
+    <svg xmlns="http://www.w3.org/2000/svg" width="1357.257" height="782.561" viewBox="0 0 1357.257 782.561" id="main-image" ref="mainSVG">
+        <g id="Group_11" data-name="Group 11" transform="translate(0 1219)">
+          <g id="migration-box" x="0" y="0" transform="translate(0, 700) scale(2)">
+            <path id="Path_1" data-name="Path 1" d="M0-584.259H275.4V-845H0Z" transform="translate(0 -132.853)" fill="red"/>
+            <g id="Group_1" data-name="Group 1" transform="translate(75.983 -938.327)">
+              <text id="Human_" data-name="Human " transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial" letter-spacing="0.001em"><tspan x="0" y="0">Human </tspan></text>
+            </g>
+            <g id="Group_2" data-name="Group 2" transform="translate(48.373 -906.452)">
+              <text id="movement_" data-name="movement " transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial"><tspan x="0" y="0">movement </tspan></text>
+            </g>
+            <g id="Group_3" data-name="Group 3" transform="translate(71.68 -874.576)">
+              <text id="labelled_" data-name="labelled " transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial" letter-spacing="0.001em"><tspan x="0" y="0">labelled </tspan></text>
+            </g>
+            <g id="Group_4" data-name="Group 4" transform="translate(41 -842.701)">
+              <text id="_" data-name="‘" transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial"><tspan x="0" y="0">‘</tspan></text>
+            </g>
+            <g id="Group_5" data-name="Group 5" transform="translate(49.527 -842.701)">
+              <text id="migration_" data-name="migration’" transform="translate(0 34)" fill="#fff" font-size="38" font-family="Arial-BoldMT, Arial" font-weight="700" letter-spacing="0.001em"><tspan x="0" y="0">migration’</tspan></text>
+            </g>
+          </g>
+          <g id="dotted-circle" opacity="0">
+              <g id="Group_6" data-name="Group 6" transform="translate(296.123 -1211.5)">
+              <path id="Path_2" data-name="Path 2" d="M0,383.781C0,171.824,171.682,0,383.462,0S766.924,171.824,766.924,383.781,595.242,767.561,383.462,767.561,0,595.737,0,383.781Z" fill="none" stroke="red" stroke-linejoin="round" stroke-width="15" stroke-dasharray="30 30"/>
+            </g>
+          </g>
+          <g id="mobility-box" x="0" y="0" transform="translate(-1500, 700) scale(2)">
+            <path id="Path_3" data-name="Path 3" d="M1696-584.259h276.042V-845H1696Z" transform="translate(-614.784 -132.853)" fill="red"/>
+            <g id="Group_7" data-name="Group 7" transform="translate(1157.596 -938.327)">
+              <text id="Human_2" data-name="Human " transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial" letter-spacing="0.001em"><tspan x="0" y="0">Human </tspan></text>
+            </g>
+            <g id="Group_8" data-name="Group 8" transform="translate(1129.985 -906.452)">
+              <text id="movement_2" data-name="movement " transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial"><tspan x="0" y="0">movement </tspan></text>
+            </g>
+            <g id="Group_9" data-name="Group 9" transform="translate(1153.293 -874.576)">
+              <text id="labelled" transform="translate(0 34)" fill="#fff" font-size="38" font-family="ArialMT, Arial" letter-spacing="0.001em"><tspan x="0" y="0">labelled</tspan></text>
+            </g>
+            <g id="Group_10" data-name="Group 10" transform="translate(1135.404 -842.701)">
+              <text id="_mobility_" data-name="‘mobility’" transform="translate(0 34)" fill="#fff" font-size="38" font-family="Arial-BoldMT, Arial" font-weight="700" letter-spacing="-0.001em"><tspan x="0" y="0">‘mobility’</tspan></text>
+            </g>
+          </g>
+        </g>
+      </svg>
     <transition-group name="fade" tag="div" class="inner-image">
       <img
-        v-for="index in totalSteps"
+        v-for="index in totalSteps - IMAGE_STEP_OFFSET"
         :key="'img'+index"
-        v-show="currentStep == index"
+        v-show="currentStep == index + IMAGE_STEP_OFFSET"
         :src="getImgUrl(index)"/>
     </transition-group>
   </div>
 </template>
 
 <script>
+const d3 = require('d3')
+
 export default {
   name: 'ChartSteps',
   props: ['currentStep', 'totalSteps'],
+  data() {
+    return {
+      IMAGE_STEP_OFFSET: 2
+    }
+  },
+  mounted () {
+    this.updateDraw()
+  },
   methods: {
     getImgUrl(index) {
       var images = require.context('../assets/images/', false, /\.svg$/)
       return images('./step' + index + ".svg")
+    },
+    updateDraw() {
+      let mainSVG = d3.select(this.$refs.mainSVG)
+      if (this.currentStep < 2) {
+          mainSVG.selectAll('#dotted-circle').transition().attr('opacity', 0)
+          mainSVG.selectAll('#migration-box').transition().attr('transform', "translate(0, 700) scale(2)")
+          mainSVG.selectAll('#mobility-box').transition().attr('transform', "translate(-1500, 700) scale(2)")
+      } else {
+        mainSVG.selectAll('#dotted-circle').transition().attr('opacity', 1)
+        mainSVG.selectAll('#migration-box').transition().attr('transform', 'scale(1)')
+        mainSVG.selectAll('#mobility-box').transition().attr('transform', 'scale(1)')
+      }
+    }
+  },
+  watch: {
+    currentStep () {
+      this.updateDraw()
     }
   },
 }
@@ -28,7 +96,7 @@ export default {
 .chart-steps
   background none
   position relative
-  img
+  #main-image
     width 100%
   .inner-image
     position absolute
@@ -40,9 +108,10 @@ export default {
     left 25%
     img
       position absolute
-      top 25%
+      top 50%
       left 0
-      transform translateY(-50%)
+      width: 100%;
+      transform translateY(-100%)
 
 .fade-enter-active,
 .fade-leave-active {
